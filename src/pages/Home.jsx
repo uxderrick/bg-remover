4f1cfb248398eeb3cdb67cfbfd4d094541957d53
UXDerrick <tsormed@gmail.com>
2024-02-06 17:51:57 +0000

    UI styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,16 +3,22 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { useState } from "react";
 import axios from "axios";
+import TheNavbar from "./Navbar";
+import { Flex, Text, colorProp } from "@radix-ui/themes";
+import Preview from "../assets/Preview.png";
+import Empty from "../assets/Empty.png";
 
 const Home = () => {
+  //set bg color to black
+  document.body.style.backgroundColor = "#000509";
+
   const [selectedFile, setSelectedFile] = useState(null);
   const [displayImageSrc, setDisplayImageSrc] = useState("");
   const [fileURL, setFileURL] = useState(
-    "https://images.unsplash.com/photo-1493612276216-ee3925520721?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    "https://images.unsplash.com/photo-1523464862212-d6631d073194?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   );
   const [fileName, setFileName] = useState("");
-
-  //
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
@@ -66,111 +72,312 @@ const Home = () => {
 
   return (
     <>
-      <div
-        className="upload-image"
+      <Flex
+        gap={"5"}
+        align={"center"}
+        direction={"column"}
         style={{
-          display: "flex",
-          justifyContent: "space-between",
-          gap: "20px",
+          padding: "20px",
+          marginTop: "120px",
         }}
       >
-        <div
+        <TheNavbar />
+        <Flex
+          direction={"column"}
+          justify={"center"}
+          align={"center"}
           style={{
-            height: "400px",
-            width: "400px",
-            backgroundColor: "lightgrey",
-            textAlign: "center",
-            paddingTop: "20px",
-            fontSize: "20px",
-            padding: "20px",
-            display: "flex",
-            justifyContent: "space-between",
-            flexDirection: "column",
-            gap: "20px",
-            borderRadius: "20px",
+            maxWidth: "680px",
+            width: "100%",
           }}
         >
-          <div
-            className="image-preview"
+          <Text
+            size={"9"}
             style={{
-              height: "100%",
-              width: "100%",
-              backgroundColor: "white",
-              textAlign: "center",
-              paddingTop: "20px",
-              fontSize: "20px",
-              padding: "20px",
-              display: "flex",
-              justifyContent: "space-between",
-              borderRadius: "10px",
-              backgroundImage: `url(${fileURL})`,
-              backgroundSize: "contain",
-              backgroundPosition: "center center",
-              backgroundRepeat: "no-repeat",
-            }}
-          ></div>
-          <>
-            <Form.Group controlId="formFile" className="">
-              <Form.Control type="file" onChange={handleFileChange} />
-            </Form.Group>
-          </>
-        </div>
-        <div
-          style={{
-            height: "400px",
-            width: "400px",
-            backgroundColor: "lightgrey",
-            textAlign: "center",
-            paddingTop: "20px",
-            fontSize: "20px",
-            padding: "20px",
-            display: "flex",
-            justifyContent: "space-between",
-            flexDirection: "column",
-            gap: "20px",
-            borderRadius: "20px",
-          }}
-        >
-          <div
-            className="image-preview"
-            style={{
-              height: "100%",
-              width: "100%",
-              backgroundColor: "white",
-              textAlign: "center",
-              //   padding: "20px",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              borderRadius: "10px",
+              color: "white",
+              fontWeight: "bolder",
             }}
           >
-            {displayImageSrc && (
-              <img
-                src={`data:image/png;base64,${displayImageSrc}`}
-                alt="displayImage"
+            {`Remove the `}
+            {
+              <Text
+                size={"9"}
                 style={{
-                  height: "100%",
-                  width: "100%",
-                  objectFit: "contain",
+                  background: `linear-gradient(90deg, #EEE73C 5%, #DE12FF 64%)`,
+                  backgroundClip: "text",
+                  WebkitTextFillColor: "transparent",
                 }}
-              />
-            )}
-          </div>
-        </div>
-      </div>
-      <Button
-        variant="outline-primary"
-        style={{
-          marginTop: "20px",
-          width: "100px",
-        }}
-        onClick={() => {
-          removeBackground(selectedFile);
-        }}
-      >
-        Generate
-      </Button>
+              >
+                background
+              </Text>
+            }
+            {` of any image`}
+          </Text>
+          <img
+            height={"180px"}
+            width={"180px"}
+            src={Preview}
+            style={{
+              position: "absolute",
+              top: "280px",
+            }}
+          ></img>
+        </Flex>
+        <Flex
+          className="upload-image"
+          justify={"center"}
+          style={{
+            display: "flex",
+            gap: "16px",
+            width: "100%",
+            height: "100%",
+            marginTop: "60px",
+          }}
+          wrap={"wrap"}
+        >
+          <Flex
+            style={{
+              // height: "100%",
+              // width: "100%",
+              maxWidth: "320px",
+              minWidth: "320px",
+              backgroundColor: "#fff",
+              padding: "8px",
+              display: "flex",
+              justifyContent: "space-between",
+              flexDirection: "column",
+              gap: "8px",
+              borderRadius: "8px",
+            }}
+          >
+            <Flex
+              className="image-preview"
+              style={{
+                height: "320px",
+                width: "100%",
+                backgroundColor: "white",
+                padding: "20px",
+                display: "flex",
+                justifyContent: "space-between",
+                borderRadius: "4px",
+                backgroundImage: `url(${fileURL})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center center",
+                backgroundRepeat: "no-repeat",
+              }}
+            ></Flex>
+            <Form.Group
+              controlId="formFile"
+              style={{
+                width: "100%",
+                display: "flex",
+                flexDirection: "row",
+                gap: "8px",
+              }}
+            >
+              <Form.Control type="file" onChange={handleFileChange} />
+              <Button
+                {...(selectedFile
+                  ? {
+                      variant: "soft",
+                      style: {
+                        width: "100px",
+                        backgroundColor: "#DE12FF",
+                        color: "white",
+                      },
+                      onClick: removeBackground,
+                    }
+                  : {
+                      variant: "soft",
+                      style: {
+                        disabled: true,
+                        width: "100px",
+                        backgroundColor: "#E4E4E9",
+                        color: "white",
+                      },
+                      onClick: () => {},
+                      disabled: true,
+                    })}
+              >
+                Generate
+              </Button>
+            </Form.Group>
+          </Flex>
+          <Flex
+            style={{
+              height: "100%",
+              width: "100%",
+              maxWidth: "320px",
+              minWidth: "320px",
+              backgroundColor: "#fff",
+              padding: "8px",
+              display: "flex",
+              justifyContent: "space-between",
+              flexDirection: "column",
+              gap: "8px",
+              borderRadius: "8px",
+            }}
+          >
+            <Flex
+              className="image-preview"
+              style={{
+                height: "320px",
+                width: "100%",
+                backgroundColor: "white",
+                textAlign: "center",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                borderRadius: "4px",
+              }}
+            >
+              {displayImageSrc ? (
+                <img
+                  src={`data:image/png;base64,${displayImageSrc}`}
+                  alt="displayImage"
+                  style={{
+                    height: "100%",
+                    width: "100%",
+                    objectFit: "cover",
+                    borderRadius: "4px",
+                  }}
+                />
+              ) : (
+                <Flex
+                  direction={"column"}
+                  align={"center"}
+                  gap={"3"}
+                  style={{
+                    width: "160px",
+                    mixBlendMode: "luminosity",
+                  }}
+                >
+                  <img
+                    src={`${Empty}`}
+                    alt="displayImage"
+                    style={{
+                      height: "40px",
+                      width: "40px",
+                      objectFit: "cover",
+                    }}
+                  />
+                  <Text
+                    size={"2"}
+                    style={{
+                      color: "#8B8D98",
+                    }}
+                  >
+                    {`Your generated image will appear here`}
+                  </Text>
+                </Flex>
+              )}
+            </Flex>
+            <Button
+              {...(displayImageSrc
+                ? {
+                    variant: "soft",
+                    style: {
+                      width: "100%",
+                      backgroundColor: "#DE12FF",
+                      color: "white",
+                    },
+                  }
+                : {
+                    variant: "soft",
+                    style: {
+                      disabled: true,
+                      width: "100%",
+                      backgroundColor: "#E4E4E9",
+                      color: "white",
+                    },
+                    onClick: () => {},
+                    disabled: true,
+                  })}
+            >
+              Download image
+            </Button>
+          </Flex>
+        </Flex>
+
+        <Flex
+          // direction={"column"}
+          // align={"center"}
+          gap={"8"}
+          style={{
+            width: "100%",
+            mixBlendMode: "luminosity",
+            marginTop: "60px",
+            backgroundColor: "#0B0B0B",
+            padding: "80px 280px",
+            borderRadius: "120px 120px 0 0",
+          }}
+        >
+          <Flex
+            direction={"column"}
+            align={"center"}
+            gap={"3"}
+            style={{
+              display: "flex",
+              gap: "8px",
+              width: "100%",
+            }}
+          >
+            <img
+              src={
+                "https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?q=80&w=2788&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              }
+              alt="displayImage"
+              style={{
+                height: "60px",
+                width: "60px",
+                objectFit: "cover",
+                borderRadius: "16px",
+              }}
+            />
+            <Text
+              size={"3"}
+              align={"center"}
+              style={{
+                color: "#8B8D98",
+              }}
+            >
+              {`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in nisi id justo aliquam aliquet. Donec id justo aliquam aliquet. Donec id justo aliquam aliquet.`}
+            </Text>
+          </Flex>
+          <Flex
+            align={"center"}
+            direction={"column"}
+            gap={"3"}
+            style={{
+              display: "flex",
+              gap: "8px",
+              width: "100%",
+            }}
+          >
+            <img
+              src={
+                "https://images.unsplash.com/photo-1493612276216-ee3925520721?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              }
+              alt="displayImage"
+              style={{
+                height: "60px",
+                width: "60px",
+                objectFit: "cover",
+                borderRadius: "16px",
+              }}
+            />
+            <Text
+              size={"3"}
+              align={"center"}
+              style={{
+                color: "#8B8D98",
+              }}
+            >
+              {`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in nisi id justo aliquam aliquet. Donec id justo aliquam aliquet. Donec id justo aliquam aliquet.`}
+            </Text>
+          </Flex>
+        </Flex>
+      </Flex>
     </>
   );
 };
---
+++ src/pages/Navbar.jsx
@@ -0,0 +1,65 @@
+/* eslint-disable no-unused-vars */
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import { Flex, Text, Button } from "@radix-ui/themes";
+
+const TheNavbar = () => {
+  return (
+    <>
+      <Navbar fixed="top" variant="dark">
+        <Container
+          style={{
+            padding: "2rem",
+            height: "80px",
+            backgroundColor: "#1D1E23",
+            borderRadius: "999px",
+            border: "1px solid #1F1F21",
+          }}
+        >
+          <Navbar.Brand href="#home">
+            <Flex gap={"2"} align={"center"}>
+              <img
+                src="https://s3-alpha-sig.figma.com/img/09a8/66c2/de9fdf8821a80d0b862c9df74ef4f0b1?Expires=1708300800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=faiZB5f1asBGrXtcSlVQ1tbo5ZfNAPPb3A2A2sRf5di6W~gc-zEozX32R1NUUGwh0R7zB38QnRpATWdifsWrmXV~zgZ8exYTZUSXovWvAevCzb3NAGumQaaNBPrDFwKtOAstc14mEX61BSlKb6Z4i16e8GPnHZwnEB~yYIdTt1PTWuKtnUQUhFJkZLnhLgppuNipvMbCxwmOJxkJye53ZY8T0RINSsIX0b0~842j2YYjE6eohMhds1Qvzk-eWL44YCVxCyTJISXX52W~z2USJODtyx13m5EaBO8ESFZjHZ~gXxZX5vOVND1eIEnr4M0Xp1U9jwx~EdwKu8Y5EEwW1Q__"
+                height={24}
+                width={24}
+              ></img>
+              <Text
+                size="4"
+                style={{
+                  fontWeight: "bold",
+                }}
+              >
+                PhotoGbee
+              </Text>
+            </Flex>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Flex gap={"2"}>
+              <Button
+                radius="full"
+                style={{
+                  backgroundColor: "#DE13FF",
+                }}
+              >
+                Log in
+              </Button>
+              <Button
+                radius="full"
+                style={{
+                  backgroundColor: "#fff",
+                  color: "#DE13FF",
+                  border: "1px solid #DE13FF",
+                }}
+              >
+                Sign up
+              </Button>
+            </Flex>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default TheNavbar;
